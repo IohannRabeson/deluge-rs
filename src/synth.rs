14a905bad390708188c28b7f25165d4a356d1e4f
irabeson <iohannrabeson@users.noreply.github.com>
2022-03-22 02:25:55 -0400

    Delete obsolete comments.

--- src/synth.rs
+++ src/synth.rs
@@ -3,10 +3,9 @@ use crate::Sound;
 #[derive(Derivative, Clone, Debug)]
 #[derivative(PartialEq)]
 pub struct Synth {
-    /// Not specified when loading a sound in a kit
     #[derivative(PartialEq = "ignore")]
     pub firmware_version: Option<String>,
-    /// Not specified when loading a sound in a kit
+    
     #[derivative(PartialEq = "ignore")]
     pub earliest_compatible_firmware: Option<String>,
 
